9a9f2865467c686a8083f6714e08ecce5a96532e
potpen <121947123+potpen@users.noreply.github.com>
2021-03-27 16:27:35 +0000

    Version 1.1r19

--- src/lib_string.c
+++ src/lib_string.c
@@ -50,4 +50,60 @@ LJLIB_ASM(string_byte)		LJLIB_REC(string_range 0)
   if (start < 0) start += len+1;
   if (start <= 0) start = 1;
   if (stop > len) stop = len;
-  if (start > stop) return FFH_
+  if (start > stop) return FFH_RES(0);  /* Empty interval: return no results. */
+  start--;
+  n = stop - start;
+  if ((uint32_t)n > LUAI_MAXCSTACK)
+    lj_err_caller(L, LJ_ERR_STRSLC);
+  lj_state_checkstack(L, (MSize)n);
+  p = (const unsigned char *)strdata(s) + start;
+  for (i = 0; i < n; i++)
+    setintV(L->base + i-1-LJ_FR2, p[i]);
+  return FFH_RES(n);
+}
+
+LJLIB_ASM(string_char)		LJLIB_REC(.)
+{
+  int i, nargs = (int)(L->top - L->base);
+  char *buf = lj_buf_tmp(L, (MSize)nargs);
+  for (i = 1; i <= nargs; i++) {
+    int32_t k = lj_lib_checkint(L, i);
+    if (!checku8(k))
+      lj_err_arg(L, i, LJ_ERR_BADVAL);
+    buf[i-1] = (char)k;
+  }
+  setstrV(L, L->base-1-LJ_FR2, lj_str_new(L, buf, (size_t)nargs));
+  return FFH_RES(1);
+}
+
+LJLIB_ASM(string_sub)		LJLIB_REC(string_range 1)
+{
+  lj_lib_checkstr(L, 1);
+  lj_lib_checkint(L, 2);
+  setintV(L->base+2, lj_lib_optint(L, 3, -1));
+  return FFH_RETRY;
+}
+
+LJLIB_CF(string_rep)		LJLIB_REC(.)
+{
+  GCstr *s = lj_lib_checkstr(L, 1);
+  int32_t rep = lj_lib_checkint(L, 2);
+  GCstr *sep = lj_lib_optstr(L, 3);
+  SBuf *sb = lj_buf_tmp_(L);
+  if (sep && rep > 1) {
+    GCstr *s2 = lj_buf_cat2str(L, sep, s);
+    lj_buf_reset(sb);
+    lj_buf_putstr(sb, s);
+    s = s2;
+    rep--;
+  }
+  sb = lj_buf_putstr_rep(sb, s, rep);
+  setstrV(L, L->top-1, lj_buf_str(L, sb));
+  lj_gc_check(L);
+  return 1;
+}
+
+LJLIB_ASM(string_reverse)  LJLIB_REC(string_op IRCALL_lj_buf_putstr_reverse)
+{
+  lj_lib_checkstr(L, 1);
+  return FFH_RE
