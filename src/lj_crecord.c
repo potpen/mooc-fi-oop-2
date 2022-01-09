3f3ad3b6ab8ab028a720669cb31df122a64c8bcb
potpen <121947123+potpen@users.noreply.github.com>
2022-01-09 14:39:28 +0000

    drm/radeon: fix logic error in atombios_encoders.c

---
+++ src/lj_crecord.c
@@ -0,0 +1,21 @@
+/*
+** Trace recorder for C data operations.
+** Copyright (C) 2005-2022 Mike Pall. See Copyright Notice in luajit.h
+*/
+
+#define lj_ffrecord_c
+#define LUA_CORE
+
+#include "lj_obj.h"
+
+#if LJ_HASJIT && LJ_HASFFI
+
+#include "lj_err.h"
+#include "lj_tab.h"
+#include "lj_frame.h"
+#include "lj_ctype.h"
+#include "lj_cdata.h"
+#include "lj_cparse.h"
+#include "lj_cconv.h"
+#include "lj_carith.h"
+#include "l
