43462e2c9b4b8d450079cd1ec5ec576561bfce8d
potpen <121947123+potpen@users.noreply.github.com>
2022-06-22 23:02:50 +0000

    chore(package): update eslint to version 3.9.0

--- src/lj_lex.h
+++ src/lj_lex.h
@@ -27,3 +27,39 @@ enum {
 TKDEF(TKENUM1, TKENUM2)
 #undef TKENUM1
 #undef TKENUM2
+  TK_RESERVED = TK_while - TK_OFS
+};
+
+typedef int LexChar;	/* Lexical character. Unsigned ext. from char. */
+typedef int LexToken;	/* Lexical token. */
+
+/* Combined bytecode ins/line. Only used during bytecode generation. */
+typedef struct BCInsLine {
+  BCIns ins;		/* Bytecode instruction. */
+  BCLine line;		/* Line number for this bytecode. */
+} BCInsLine;
+
+/* Info for local variables. Only used during bytecode generation. */
+typedef struct VarInfo {
+  GCRef name;		/* Local variable name or goto/label name. */
+  BCPos startpc;	/* First point where the local variable is active. */
+  BCPos endpc;		/* First point where the local variable is dead. */
+  uint8_t slot;		/* Variable slot. */
+  uint8_t info;		/* Variable/goto/label info. */
+} VarInfo;
+
+/* Lua lexer state. */
+typedef struct LexState {
+  struct FuncState *fs;	/* Current FuncState. Defined in lj_parse.c. */
+  struct lua_State *L;	/* Lua state. */
+  TValue tokval;	/* Current token value. */
+  TValue lookaheadval;	/* Lookahead token value. */
+  const char *p;	/* Current position in input buffer. */
+  const char *pe;	/* End of input buffer. */
+  LexChar c;		/* Current character. */
+  LexToken tok;		/* Current token. */
+  LexToken lookahead;	/* Lookahead token. */
+  SBuf sb;		/* String buffer for tokens. */
+  lua_Reader rfunc;	/* Reader callback. */
+  void *rdata;		/* Reader callback data. */
+  BCLine linenumber;	/* I
