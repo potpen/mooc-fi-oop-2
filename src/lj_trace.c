740d84e887d88869962ea801e880e03531336794
potpen <121947123+potpen@users.noreply.github.com>
2022-10-21 22:38:56 +0000

    Added i18n

--- src/lj_trace.c
+++ src/lj_trace.c
@@ -886,4 +886,51 @@ int LJ_FASTCALL lj_trace_exit(jit_State *J, void *exptr)
     T = traceref(J, J->parent);
   }
 #endif
-  lj_assertJ(T != NULL && J->exitno < T->nsnap,
+  lj_assertJ(T != NULL && J->exitno < T->nsnap, "bad trace or exit number");
+  exd.J = J;
+  exd.exptr = exptr;
+  errcode = lj_vm_cpcall(L, NULL, &exd, trace_exit_cp);
+  if (errcode)
+    return -errcode;  /* Return negated error code. */
+
+  if (exitcode) copyTV(L, L->top++, &exiterr);  /* Anchor the error object. */
+
+  if (!(LJ_HASPROFILE && (G(L)->hookmask & HOOK_PROFILE)))
+    lj_vmevent_send(L, TEXIT,
+      lj_state_checkstack(L, 4+RID_NUM_GPR+RID_NUM_FPR+LUA_MINSTACK);
+      setintV(L->top++, J->parent);
+      setintV(L->top++, J->exitno);
+      trace_exit_regs(L, ex);
+    );
+
+  pc = exd.pc;
+  cf = cframe_raw(L->cframe);
+  setcframe_pc(cf, pc);
+  if (exitcode) {
+    return -exitcode;
+  } else if (LJ_HASPROFILE && (G(L)->hookmask & HOOK_PROFILE)) {
+    /* Just exit to interpreter. */
+  } else if (G(L)->gc.state == GCSatomic || G(L)->gc.state == GCSfinalize) {
+    if (!(G(L)->hookmask & HOOK_GC))
+      lj_gc_step(L);  /* Exited because of GC: drive GC forward. */
+  } else {
+    trace_hotside(J, pc);
+  }
+  if (bc_op(*pc) == BC_JLOOP) {
+    BCIns *retpc = &traceref(J, bc_d(*pc))->startins;
+    int isret = bc_isret(bc_op(*retpc));
+    if (isret || bc_op(*retpc) == BC_ITERN) {
+      if (J->state == LJ_TRACE_RECORD) {
+	J->patchins = *pc;
+	J->patchpc = (BCIns *)pc;
+	*J->patchpc = *retpc;
+	J->bcskip = 1;
+      } else if (isret) {
+	pc = retpc;
+	setcframe_pc(cf, pc);
+      }
+    }
+  }
+  /* Return MULTRES or 0. */
+  ERRNO_RESTORE
+  switch (
